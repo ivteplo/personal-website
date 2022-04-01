9b437c823be51e4481081287052541b51dcd7b2c
ivteplo <iteplov7@gmail.com>
2022-04-01 22:13:18 +0300

    Add custom fonts

---
+++ pages/_document.js
@@ -0,0 +1,25 @@
+// Copyright (c) 2022 Ivan Teplov
+
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Poppins font */}
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
