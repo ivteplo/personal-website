af1a52a955efad1568b2f8a73966689dfe47925b
ivteplo <iteplov7@gmail.com>
2022-04-22 23:03:23 +0300

    Add Open Graph tags for project pages

--- apps/website/components/projects/ProjectPage.js
+++ apps/website/components/projects/ProjectPage.js
@@ -7,10 +7,13 @@ import ProjectHeader from './ProjectHeader'
 import ProjectRepository from './ProjectRepository'
 import ProjectTechnologies from './ProjectTechnologies'
 
+import previewImagePath from './_previewImagePath'
+
 export default function ProjectPage({
   project = {
     name: '',
     fullName: '',
+    description: '',
     path: '',
     technologies: [],
     repository: '',
@@ -22,6 +25,14 @@ export default function ProjectPage({
     <>
       <Head>
         <title>{project.fullName} | Projects | Ivan Teplov</title>
+        <meta
+          name="og:title"
+          content={`${project.fullName} | Projects | Ivan Teplov`}
+        />
+        <meta name="og:description" content={project.description} />
+        <meta name="og:type" content="article" />
+        <meta name="og:image" content={previewImagePath(project.path)} />
+        <meta name="og:url" content="https://ivanteplov.vercel.app" />
       </Head>
       <ProjectHeader
         projectName={project.name}
